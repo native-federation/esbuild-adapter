25e63359c21cb36d7a6bdee6bcb54f56a8317f11
Aukevanoost <auke.v.o@hotmail.com>
2026-05-24 13:16:15 +0200

    chore: moved react to single file

--- packages/adapter/src/lib/frameworks/react.ts
+++ packages/adapter/src/lib/frameworks/react.ts
@@ -1,5 +1,36 @@
 import type { NfFrameworkPlugin } from '../domain/framework-plugin.contract.js';
-import { reactReplacements } from '../utils/react-replacements.js';
+import { type ReplacementConfig } from '../domain/adapter-config.contract.js';
+
+export const reactReplacements: Record<string, Record<string, ReplacementConfig>> = {
+  dev: {
+    'node_modules/react/index.js': {
+      file: 'node_modules/react/cjs/react.development.js',
+    },
+    'node_modules/react/jsx-dev-runtime.js': {
+      file: 'node_modules/react/cjs/react-jsx-dev-runtime.development.js',
+    },
+    'node_modules/react/jsx-runtime.js': {
+      file: 'node_modules/react/cjs/react-jsx-runtime.development.js',
+    },
+    'node_modules/react-dom/index.js': {
+      file: 'node_modules/react-dom/cjs/react-dom.development.js',
+    },
+  },
+  prod: {
+    'node_modules/react/index.js': {
+      file: 'node_modules/react/cjs/react.production.min.js',
+    },
+    'node_modules/react/jsx-dev-runtime.js': {
+      file: 'node_modules/react/cjs/react-jsx-dev-runtime.production.min.js',
+    },
+    'node_modules/react/jsx-runtime.js': {
+      file: 'node_modules/react/cjs/react-jsx-runtime.production.min.js',
+    },
+    'node_modules/react-dom/index.js': {
+      file: 'node_modules/react-dom/cjs/react-dom.production.min.js',
+    },
+  },
+};
 
 export function reactFrameworkPlugin(): NfFrameworkPlugin {
   return {
--- packages/adapter/src/lib/utils/index.ts
+++ packages/adapter/src/lib/utils/index.ts
@@ -2,4 +2,3 @@ export * from './write-result.js';
 export * from './node-modules-bundler.js';
 export * from './source-code-bundler.js';
 export * from './collect-exports.js';
-export * from './react-replacements.js';
